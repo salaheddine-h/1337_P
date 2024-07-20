14c9da2609751e82eefd0de17869f430896a3aef
salaheddine-h <101067974+salaheddine-h@users.noreply.github.com>
2024-07-20 13:49:36 +0200

    Update ft_range.c

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_range.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: imiqor <imiqor@student.42.fr>              +#+  +:+       +#+        */
+/*   By: salhali <salhali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 13:26:34 by imiqor            #+#    #+#             */
 /*   Updated: 2024/07/14 20:57:35 by imiqor           ###   ########.fr       */
